5312d0be3e5b1eadcaf7e068fe20e0e9b4319702
MathiasRobert <matrobert@ensc.fr>
2017-10-25 17:13:45 +0200

    test

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,8 +48,6 @@ class LoginController extends Controller
         $client->setAccessToken(json_encode($google_client_token));
 
         $authUser = $this->findOrCreateUser($user);
-        dd($client);
-
         Auth::login($authUser, 1);
         dd($authUser);
 
