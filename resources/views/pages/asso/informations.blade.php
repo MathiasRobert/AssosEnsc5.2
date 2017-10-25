e32cf75a2c2480b444449f2812861e4a3e28188e
MathiasRobert <matrobert@ensc.fr>
2017-10-25 17:25:40 +0200

    surnom

--- resources/views/pages/asso/informations.blade.php
+++ resources/views/pages/asso/informations.blade.php
@@ -18,7 +18,7 @@
                     </div>
                     <div class="card-block">
                         <h3 class="card-title">{!! $m->prenom !!} {!! $m->nom !!}</h3>
-                        <h6 class="category text-primary">{!! $m->poste !!}</h6>
+                        <h6 class="category text-primary">{!! $m->poste !!} - <i>{!! $m->surnom !!}</i></h6>
                         <p class="card-description">
                             {!! $m->description !!}
                         </p>
