618d44e558e037ce83f180fc2ebd13bba66c8f75
MathiasRobert <matrobert@ensc.fr>
2017-10-27 19:57:59 +0200

    nouvel onglet

--- resources/views/pages/asso/informations.blade.php
+++ resources/views/pages/asso/informations.blade.php
@@ -23,7 +23,7 @@
                             {!! $m->description !!}
                         </p>
                         <div class="card-footer">
-                            <a href="{!! $m->lien_facebook !!}" class="btn btn-icon btn-primary btn-round"><i class="fa fa-facebook-square"></i></a>
+                            <a href="{!! $m->lien_facebook !!}" target="_blank" class="btn btn-icon btn-primary btn-round"><i class="fa fa-facebook-square"></i></a>
                             <a href="mailto:{!! $m->email !!}" target="_blank" class="btn btn-icon btn-primary btn-round"><i class="now-ui-icons ui-1_email-85"></i></a>
                         </div>
                     </div>
